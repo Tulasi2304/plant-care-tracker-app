d42e87eea882f6ef15ac1d7dfde05f4189ceca3d
sushma1031 <sushmajram@gmail.com>
2024-07-15 19:20:33 +0530

    Port static pages to server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 3000
+const HTML = __dirname + "/public/html/"
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.sendFile(`${HTML}index.html`);
 })
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${HTML}about.html`);
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(`${HTML}contact.html`);
+});
+
+app.get("/plants", (req, res) => {
+  res.sendFile(`${HTML}plants.html`);
+});
 
 app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
